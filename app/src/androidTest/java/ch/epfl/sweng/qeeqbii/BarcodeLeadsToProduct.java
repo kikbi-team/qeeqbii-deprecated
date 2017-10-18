8e1121fdf847ad78d15b5bca6749637793488a3f
Sergei Volodin <etoestja@yandex.ru>
2017-10-18 21:43:03 +0200

    Test valid and invalid (empty) barcodes behavior

---
+++ app/src/androidTest/java/ch/epfl/sweng/qeeqbii/BarcodeLeadsToProduct.java
@@ -0,0 +1,42 @@
+package ch.epfl.sweng.qeeqbii;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+
+@RunWith(AndroidJUnit4.class)
+public class BarcodeLeadsToProduct {
+    @Rule
+    public IntentsTestRule<BarcodeActivity> mActivityRule =
+            new IntentsTestRule<BarcodeActivity>(BarcodeActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        BarcodeActivity activity = mActivityRule.getActivity();
+        activity.processBarcode("7611654884033");
+        intended(hasComponent(new ComponentName(getTargetContext(), BarcodeToProductActivity.class)));
+    }
+}
