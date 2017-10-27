3c57dfaf34dca75e3d82d214519018d411f77e7c
dcleres <david.cleres@epfl.ch>
2017-10-28 00:42:36 +0200

    new tests for the CSV

--- app/src/androidTest/java/ch/epfl/sweng/qeeqbii/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/qeeqbii/MainActivityTest.java
@@ -2,17 +2,22 @@ package ch.epfl.sweng.qeeqbii;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    /*
+
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
@@ -35,7 +40,6 @@ public class MainActivityTest {
     public void testCanOpenGraphs() throws InterruptedException {
         onView(withId(R.id.buttonGraphs)).perform(click());
     }
-    */
 
     @Test
     public void useAppContext() throws Exception {
