f7e51c9f5b168061e62d552e4f7f874e82069a70
dcleres <david.cleres@epfl.ch>
2017-10-31 14:54:17 +0100

    new interface and list feature starts to be implemented

---
+++ app/src/main/java/ch/epfl/sweng/qeeqbii/InteractiveArrayAdapter.java
@@ -0,0 +1,66 @@
+package ch.epfl.sweng.qeeqbii;
+
+/**
+ * Created by davidcleres on 31.10.17.
+ */
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class InteractiveArrayAdapter extends ArrayAdapter<ModelForShoppingCart> {
+
+        private final List<ModelForShoppingCart> list;
+        private final Activity context;
+
+        public InteractiveArrayAdapter(Activity context, List<ModelForShoppingCart> list) {
+            super(context, R.layout.content_shopping_cart, list);
+            this.context = context;
+            this.list = list;
+        }
+
+        static class ViewHolder {
+            protected TextView text;
+            protected CheckBox checkbox;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = null;
+            if (convertView == null) {
+                LayoutInflater inflator = context.getLayoutInflater();
+                view = inflator.inflate(R.layout.content_shopping_cart, null);
+                final ViewHolder viewHolder = new ViewHolder();
+                viewHolder.text = (TextView) view.findViewById(R.id.label);
+                viewHolder.checkbox = (CheckBox) view.findViewById(R.id.check);
+                viewHolder.checkbox
+                        .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView,
+                                                         boolean isChecked) {
+                                ModelForShoppingCart element = (ModelForShoppingCart) viewHolder.checkbox
+                                        .getTag();
+                                element.setSelected(buttonView.isChecked());
+
+                            }
+                        });
+                view.setTag(viewHolder);
+                viewHolder.checkbox.setTag(list.get(position));
+            } else {
+                view = convertView;
+                ((ViewHolder) view.getTag()).checkbox.setTag(list.get(position));
+            }
+            ViewHolder holder = (ViewHolder) view.getTag();
+            holder.text.setText(list.get(position).getName());
+            holder.checkbox.setChecked(list.get(position).isSelected());
+            return view;
+        }
+    }
--- app/src/main/java/ch/epfl/sweng/qeeqbii/ShoppingCart.java
+++ app/src/main/java/ch/epfl/sweng/qeeqbii/ShoppingCart.java
@@ -1,5 +1,6 @@
 package ch.epfl.sweng.qeeqbii;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingCart extends AppCompatActivity {
 
@@ -17,6 +22,11 @@ public class ShoppingCart extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //ListActivity
+        //ArrayAdapter<ModelForShoppingCart> adapter = new InteractiveArrayAdapter(this,
+        //        getModel());
+        //setListAdapter(adapter);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,12 +34,30 @@ public class ShoppingCart extends AppCompatActivity {
                 showShoppingList(view);
             }
         });
-
-
     }
+
     public void showShoppingList(View view) {
         Intent intent = new Intent(this, ShoppingList.class);
         startActivity(intent);
+    }
+
+    private List<ModelForShoppingCart> getModel() {
+        List<ModelForShoppingCart> list = new ArrayList<ModelForShoppingCart>();
+        list.add(get("Linux"));
+        list.add(get("Windows7"));
+        list.add(get("Suse"));
+        list.add(get("Eclipse"));
+        list.add(get("Ubuntu"));
+        list.add(get("Solaris"));
+        list.add(get("Android"));
+        list.add(get("iPhone"));
+        // Initially select one of the items
+        list.get(1).setSelected(true);
+        return list;
+    }
+
+    private ModelForShoppingCart get(String s) {
+        return new ModelForShoppingCart(s);
     }
 
 }
