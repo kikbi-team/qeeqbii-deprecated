61730ab909c2ce930b441cc08ef0a25f772503cf
dcleres <david.cleres@epfl.ch>
2017-11-14 19:59:08 +0100

    Corrections

--- app/src/main/java/ch/epfl/sweng/qeeqbii/ShoppingCartStatistics.java
+++ app/src/main/java/ch/epfl/sweng/qeeqbii/ShoppingCartStatistics.java
@@ -113,7 +113,7 @@ public class ShoppingCartStatistics extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 2;
+            return 3;
         }
 
         @Override
