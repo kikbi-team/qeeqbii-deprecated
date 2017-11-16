1de8b3b794f25d6604eb7333f466021e4637a485
Guillaume Mollard <guillaume.mollard2@gmail.com>
2017-11-16 17:46:25 +0000

    BarcodeLeadsToProduct test modified

--- app/src/androidTest/java/ch/epfl/sweng/qeeqbii/ARecyclerViewAdapterShould.java
+++ app/src/androidTest/java/ch/epfl/sweng/qeeqbii/ARecyclerViewAdapterShould.java
@@ -26,7 +26,7 @@ public class ARecyclerViewAdapterShould {
         Date date = new Date();
         Product item = new Product("cheese", "500 mg", "Stuff", "cool Nutrients", "001", R.drawable.cheese);
         List<Product> products = new ArrayList<Product>();
-        products.add(item); 
+        products.add(item);
         products.add(item);
         products.add(item);
 
