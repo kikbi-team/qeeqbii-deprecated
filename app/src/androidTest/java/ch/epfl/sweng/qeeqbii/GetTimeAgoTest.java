bf3fb9cf57053fb8de14f10fcb376214d064255c
Nicolas Lesimple <nicolas.lesimple@epfl.ch>
2017-11-28 09:12:58 +0100

    don t understand why test fail

--- app/src/androidTest/java/ch/epfl/sweng/qeeqbii/GetTimeAgoTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/qeeqbii/GetTimeAgoTest.java
@@ -20,7 +20,7 @@ public class GetTimeAgoTest {
 
         String test = getTimeAgo(time,cxt);
         long now = System.currentTimeMillis();
-        assertEquals(test,"17497 days ago");
+        assertEquals(test,"17498 days ago");
     }
 
 }
