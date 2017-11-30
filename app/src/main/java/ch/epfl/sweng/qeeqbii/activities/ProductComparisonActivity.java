ad70e5ea1cf9dade236dfe48d0cbc680f745dc2a
Sergei Volodin <etoestja@yandex.ru>
2017-11-30 22:44:01 +0100

    Add ListView of text, chart

--- app/src/main/java/ch/epfl/sweng/qeeqbii/activities/ProductComparisonActivity.java
+++ app/src/main/java/ch/epfl/sweng/qeeqbii/activities/ProductComparisonActivity.java
@@ -1,85 +1,46 @@
 package ch.epfl.sweng.qeeqbii.activities;
 
+/*
+Created by sergei on 30 Nov 2017
+
+This class implements a product comparison activity
+ */
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
 
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.BarLineScatterCandleBubbleData;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-
 import ch.epfl.sweng.qeeqbii.R;
-
+import ch.epfl.sweng.qeeqbii.activities.comparison.ComparisonGraphAdapter;
+import ch.epfl.sweng.qeeqbii.activities.comparison.ProductsLine;
 
 
 public class ProductComparisonActivity extends AppCompatActivity {
 
-    public class ProductsLine {
-        public String criteria;
-        public Double value1;
-        public Double value2;
-        ProductsLine(String criteria_, Double val1, Double val2) {
-            criteria = criteria_;
-            value1 = val1;
-            value2 = val2;
-        }
-    }
+    private ComparisonGraphAdapter adapter = null;
 
-    public class LabelFormatter implements IAxisValueFormatter {
-        private final ArrayList<String> mLabels;
-
-        public LabelFormatter(ArrayList<String> labels) {
-            mLabels = labels;
-        }
-
-        @Override
-        public String getFormattedValue(float value, AxisBase axis) {
-            int idx = (int) value;
-            if(mLabels != null && (float) idx - value < 1e-2)
-                return mLabels.get(idx);
-            else return "";
-        }
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_comparison);
-
-        HorizontalBarChart mChart = (HorizontalBarChart) findViewById(R.id.chart1);
-        setData(mChart, new ProductsLine("Salt (g)", 100., 200.));
-    }
-
-
-    private void setData(HorizontalBarChart chart, ProductsLine line) {
+    // add data to a bar chart
+    public static void setData(HorizontalBarChart chart, ProductsLine line) {
         ArrayList<ProductsLine> lines = new ArrayList<ProductsLine>();
         lines.add(line);
         setData(chart, lines);
-        chart.getDescription().setText(line.criteria);
     }
 
-    private void setData(HorizontalBarChart chart, ArrayList<ProductsLine> lines) {
-
+    // add multiple lines to a bar chart
+    private static void setData(HorizontalBarChart chart, ArrayList<ProductsLine> lines) {
         chart.getXAxis().setPosition(XAxis.XAxisPosition.TOP);
         chart.getDescription().setText("");
 
@@ -92,14 +53,14 @@ public class ProductComparisonActivity extends AppCompatActivity {
         final int line_n = lines.size();
 
         ArrayList<String> labels = new ArrayList<String>();
-        for(int i = 0; i < line_n * bars_per_line; i++)
+        for (int i = 0; i < line_n * bars_per_line; i++)
             labels.add("");
 
         ArrayList<BarEntry> valueSet1 = new ArrayList<>();
         ArrayList<BarEntry> valueSet2 = new ArrayList<>();
 
         int i = 0;
-        for(ProductsLine line: lines) {
+        for (ProductsLine line : lines) {
             valueSet1.add(new BarEntry(i * bars_per_line, line.value1.floatValue()));
             valueSet2.add(new BarEntry(i * bars_per_line + 1, line.value2.floatValue()));
             labels.set(i * bars_per_line, line.criteria);
@@ -117,10 +78,59 @@ public class ProductComparisonActivity extends AppCompatActivity {
 
         LabelFormatter formatter = new LabelFormatter(line_n > 1 ? labels : null);
         chart.getXAxis().setValueFormatter(formatter);
+
+        chart.setDrawGridBackground(false);
+        chart.getXAxis().setDrawGridLines(false);
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getAxisRight().setDrawGridLines(false);
+        chart.getAxisRight().setDrawLabels(false);
+
+        if (line_n == 1)
+            chart.getDescription().setText(lines.get(0).criteria);
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_comparison);
+
+        adapter = new ComparisonGraphAdapter();
+        adapter.setContext(getApplicationContext());
+        adapter.addLine(new ProductsLine("Salt (g)", 100., 200.));
+        adapter.addLine(new ProductsLine("Energy (J)", 5000., 6000.));
+        adapter.addLine(new ProductsLine("Salt (g)", 100., 200.));
+        adapter.addLine(new ProductsLine("Energy (J)", 5000., 6000.));
+        adapter.addLine(new ProductsLine("Salt (g)", 100., 200.));
+        adapter.addLine(new ProductsLine("Energy (J)", 5000., 6000.));
+        adapter.addLine(new ProductsLine("Salt (g)", 100., 200.));
+        adapter.addLine(new ProductsLine("Energy (J)", 5000., 6000.));
+        adapter.addLine(new ProductsLine("Salt (g)", 100., 200.));
+        adapter.addLine(new ProductsLine("Energy (J)", 5000., 6000.));
+        ListView list = (ListView) findViewById(R.id.graphs);
+        list.setAdapter(adapter);
+
+    }
+
+    // scan barcode button handler
     public void scanBarcode(View w) {
         Intent intent = new Intent(this, BarcodeScannerActivity.class);
         startActivity(intent);
+    }
+
+    // formatter for axis which outputs pre-defined values
+    public static class LabelFormatter implements IAxisValueFormatter {
+        private final ArrayList<String> mLabels;
+
+        public LabelFormatter(ArrayList<String> labels) {
+            mLabels = labels;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            int idx = (int) value;
+            if (mLabels != null && (float) idx - value < 1e-2)
+                return mLabels.get(idx);
+            else return "";
+        }
     }
 }
---
+++ app/src/main/java/ch/epfl/sweng/qeeqbii/activities/comparison/ProductsLine.java
@@ -0,0 +1,24 @@
+package ch.epfl.sweng.qeeqbii.activities.comparison;
+
+/**
+ * Created by sergei on 11/30/17.
+ */
+
+
+// This class holds a single line (by single nutrient) in the ProductComparison activity
+public class ProductsLine {
+    // nutrient
+    public String criteria;
+
+    // value for product 1
+    public Double value1;
+
+    // value for product 2
+    public Double value2;
+
+    public ProductsLine(String criteria_, Double val1, Double val2) {
+        criteria = criteria_;
+        value1 = val1;
+        value2 = val2;
+    }
+}
