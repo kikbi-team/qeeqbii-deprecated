41d6374684e557102de56a928704f6f312d521fc
Sergei Volodin <etoestja@yandex.ru>
2017-12-07 22:10:39 +0100

    Add ProductAddActivity layout

--- app/src/main/java/ch/epfl/sweng/qeeqbii/activities/ProductAddActivity.java
+++ app/src/main/java/ch/epfl/sweng/qeeqbii/activities/ProductAddActivity.java
@@ -1,9 +1,14 @@
 package ch.epfl.sweng.qeeqbii.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import ch.epfl.sweng.qeeqbii.R;
+
+import static ch.epfl.sweng.qeeqbii.activities.BarcodeScannerActivity.EXTRA_BARCODE;
 
 public class ProductAddActivity extends AppCompatActivity {
 
@@ -11,5 +16,19 @@ public class ProductAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_add);
+
+        // obtaining barcode from extras
+        Intent intent = getIntent();
+        String barcode = intent.getStringExtra(EXTRA_BARCODE);
+
+        // setting it to the textview if it is not null
+        if(barcode != null) {
+            Log.d("STATE", "Found " + barcode + " in extras");
+            final TextView barcode_field = (TextView) findViewById(R.id.view_add_barcode);
+            barcode_field.setText(barcode);
+        }
+        else {
+            Log.d("STATE", "No barcode in extras");
+        }
     }
 }
