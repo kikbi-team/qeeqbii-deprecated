c626410955e61969e3fc187b8871f49fe04119d6
Sergei Volodin <etoestja@yandex.ru>
2017-12-18 18:14:25 +0100

    Refactor Slider.java; Close the drawer on menu item selection

--- app/src/main/java/ch/epfl/sweng/qeeqbii/Slider.java
+++ app/src/main/java/ch/epfl/sweng/qeeqbii/Slider.java
@@ -1,8 +1,14 @@
 package ch.epfl.sweng.qeeqbii;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.MenuItem;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.epfl.sweng.qeeqbii.activities.BarcodeScannerActivity;
 import ch.epfl.sweng.qeeqbii.activities.CancerDataQueryActivity;
@@ -20,64 +26,106 @@ import ch.epfl.sweng.qeeqbii.chat.MainActivityChat;
  * Created by adrien on 07.12.17.
  */
 
-public class Slider  {
+public class Slider {
+
+    // mapping menu item id -> activity class name
+    private static Map<Integer, Class> activityNames;
+
+    // mapping current activity class -> slider drawer id
+    private static Map<Class, Integer> drawerIds;
+
+    // true if mappings are filled
+    private static Boolean mIsInisizlied = false;
+
+    // fill the mappings if it is neccessary
+    private static void initialize() {
+        if (!mIsInisizlied) {
+            activityNames = new HashMap<>();
+            drawerIds = new HashMap<>();
+
+            // adding activity names to their slider ids
+            activityNames.put(R.id.nav_main, MainActivity.class);
+            activityNames.put(R.id.nav_chat, MainActivityChat.class);
+            activityNames.put(R.id.nav_graphs, GraphsActivity.class);
+            activityNames.put(R.id.nav_cancerdb, CancerDataShowActivity.class);
+            activityNames.put(R.id.nav_scan, BarcodeScannerActivity.class);
+            activityNames.put(R.id.nav_shopping_cart, ShoppingListActivity.class);
+            activityNames.put(R.id.nav_dataquery, CancerDataQueryActivity.class);
+            activityNames.put(R.id.nav_scannedProducts, RecentlyScannedProductsActivity.class);
+            activityNames.put(R.id.nav_scanning_history, SavedProductsDatesActivity.class);
+            activityNames.put(R.id.nav_stats, StatisticsActivity.class);
+            activityNames.put(R.id.nav_comparison, ProductComparisonActivity.class);
 
 
+            // adding slider drawer ids
+            drawerIds.put(MainActivity.class, R.id.drawer_main);
+            //drawerIds.put(MainActivityChat.class, None);
+            drawerIds.put(GraphsActivity.class, R.id.GraphsLayout);
+            drawerIds.put(CancerDataShowActivity.class, R.id.cancerDataShow);
+            drawerIds.put(BarcodeScannerActivity.class, R.id.barcode_scanner);
+            //drawerIds.put(ShoppingListActivity.class, None);
+            drawerIds.put(CancerDataQueryActivity.class, R.id.cancer_query_design);
+            drawerIds.put(RecentlyScannedProductsActivity.class, R.id.SavedProductsLayout);
+            drawerIds.put(SavedProductsDatesActivity.class, R.id.SavedProductsLayout);
+            //drawerIds.put(StatisticsActivity.class, None);
+            drawerIds.put(ProductComparisonActivity.class, R.id.ComparisonLayout);
+
+            mIsInisizlied = true;
+        }
+    }
+
+    // called on click in slider
     public void goToActivity(MenuItem item, Context context) {
-        if (item.getItemId() == R.id.nav_main) {
-            Intent intent = new Intent(context, MainActivity.class);
+
+        // initialize mappings on start
+        // done only once
+        initialize();
+
+        // requested menu item id
+        Integer requested_id = item.getItemId();
+
+        // checking if name is known
+        if (activityNames.containsKey(requested_id)) {
+
+            // obtaining calling activity
+            Activity currentActivity = (Activity) context;
+
+            // checking if current activity has a drawer to close it
+            if (drawerIds.containsKey(currentActivity.getClass())) {
+                // obtaining drawer id
+                Integer drawerId = drawerIds.get(currentActivity.getClass());
+
+                // obtaining the drawer
+                DrawerLayout mDrawerLayout = (DrawerLayout) currentActivity.findViewById(drawerId);
+
+                // closing the drawer
+                if (mDrawerLayout != null) {
+                    Log.d("STATE", "Closing drawer");
+                    mDrawerLayout.closeDrawers();
+                } else {
+                    Log.d("STATE", "Drawer is null");
+                }
+            } else {
+                Log.d("STATE", "Drawer not detected");
+            }
+
+            // obtaining activity class
+            Class activity_class = activityNames.get(requested_id);
+
+            Log.d("STATE", "Slider: going to " + activity_class);
+
+            // launching new intent
+            Intent intent = new Intent(context, activity_class);
+
+            // if the activity exists, just bring it to front
             intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+
+            // start activity
             context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_chat) {
-            Intent intent = new Intent(context, MainActivityChat.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_graphs) {
-            Intent intent = new Intent(context, GraphsActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_cancerdb) {
-            Intent intent = new Intent(context, CancerDataShowActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_scan) {
-            Intent intent = new Intent(context, BarcodeScannerActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_shopping_cart) {
-            Intent intent = new Intent(context, ShoppingListActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_dataquery) {
-            Intent intent = new Intent(context, CancerDataQueryActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_scannedProducts) {
-            Intent intent = new Intent(context, RecentlyScannedProductsActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_scanning_history) {
-            Intent intent = new Intent(context, SavedProductsDatesActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_stats) {
-            Intent intent = new Intent(context, StatisticsActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.nav_comparison) {
-            Intent intent = new Intent(context, ProductComparisonActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            context.startActivity(intent);
+
+        } else {
+            // the pressed menu item is unknown to this class
+            Log.d("STATE", "Slider: unknown menu id " + requested_id);
         }
     }
 }
